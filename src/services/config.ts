796eac9dc27d1736492afbe43825ec1f98ec2528
ginlink <ginlink1998@gmail.com>
2022-05-04 11:31:30 +0800

    feat: add multi env

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,8 +1,1 @@
-export const isDev = process.env.NODE_ENV === 'development'
-
-const BASE_URL_DEV = 'http://localhost:3050/api'
-// const BASE_URL_PRO = 'https://api.gincool.com/api'
-
-// export const BASE_URL = isDev ? BASE_URL_DEV : BASE_URL_PRO
-// export const BASE_URL = BASE_URL_PRO
-export const BASE_URL = BASE_URL_DEV
+export const BASE_URL = process.env.REACT_APP_BASE_URL
